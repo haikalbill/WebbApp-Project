cf817e5875f03932b8d7cf9893ebbbfcc2195d49
haikalbill <90669152+haikalbill@users.noreply.github.com>
2023-01-23 22:09:07 +0800

    Add files via upload

--- mainpage.blade.php
+++ mainpage.blade.php
@@ -1,4 +1,4 @@
-@extends('master.layout')
+@extends('master.mainpagelayout')
 @section('content')
 
   <!-- ======= Hero Section ======= -->
@@ -7,9 +7,8 @@
       <div class="row justify-content-between gy-5">
         <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
           <h2 data-aos="fade-up">No one has ever  <br>become poor from giving</h2>
-          <p data-aos="fade-up" data-aos-delay="100">Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+          <p data-aos="fade-up" data-aos-delay="100">GoodFood is a non-profit organization which aims to distribute nutritious food for those in need. </p>
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-            <a href="#book-a-table" class="btn-book-a-table">Donate Now</a>
             <a href="https://www.youtube.com/watch?v=1ZcYwbqtVtc" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
           </div>
         </div>
@@ -32,31 +31,26 @@
         </div>
 
         <div class="row gy-4">
-          <div class="col-lg-7 position-relative about-img" style="background-image: url(assets/img/about.jpg) ;" data-aos="fade-up" data-aos-delay="150">
-            <div class="call-us position-absolute">
-              <h4>Donate Now</h4>
-              <p>+1 5589 55488 55</p>
-            </div>
+          <div class="col-lg-7 position-relative about-img" style="background-image: url(assets/img/Centara-food-donation-to-PRD_02.jpg) ;" data-aos="fade-up" data-aos-delay="150">
+
           </div>
           <div class="col-lg-5 d-flex align-items-end" data-aos="fade-up" data-aos-delay="300">
             <div class="content ps-0 ps-lg-5">
               <p class="fst-italic">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua.
+                For many donors, donating is a platform for knowing about the issues surrounding that particular need. Most people choose to learn about the issues, before donating, to get a wider perspective.
               </p>
               <ul>
-                <li><i class="bi bi-check2-all"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                <li><i class="bi bi-check2-all"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                <li><i class="bi bi-check2-all"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                <li><i class="bi bi-check2-all"></i> Increased security</li>
+                <li><i class="bi bi-check2-all"></i> Immediate engagement opportunities</li>
+                <li><i class="bi bi-check2-all"></i> Giving the donors the freedom to choose how, when, and how much they give will set up your nonprofit fundraising goals to succeed.</li>
               </ul>
               <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                Our objective in creating this website is so that we can reach out to as many people across the globe to donate to the charity for the one who needs food the most. In addition to that, we are hoping that we can help other human beings no matter who and where they are. By doing so, we can reduce the hunger problem in this world.
               </p>
 
               <div class="position-relative mt-4">
-                <img src="assets/img/about-2.jpg" class="img-fluid" alt="">
-                <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox play-btn"></a>
+                <img src="assets/img/d41586-021-02331-x_19590764.jpg" class="img-fluid" alt="">
+                <a href="https://youtu.be/cYpemn-5lTU" class="glightbox play-btn"></a>
               </div>
             </div>
           </div>
@@ -75,8 +69,7 @@
             <div class="why-box">
               <h3>Why Choose GoodFood?</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
+                In relation to Sustainable Goal Development, Goal 2: Zero Hunger, we have decided to create a platform where everyone is able to help with our cause as our motto is “to care is to share”. With this initiative, it will come in a lot of handy for people to donate food to people that need food across the globe.
               </p>
               <div class="text-center">
                 <a href="#" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
@@ -90,24 +83,24 @@
               <div class="col-xl-4" data-aos="fade-up" data-aos-delay="200">
                 <div class="icon-box d-flex flex-column justify-content-center align-items-center">
                   <i class="bi bi-clipboard-data"></i>
-                  <h4>Corporis voluptates officia eiusmod</h4>
-                  <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                  <h4>Always Open for Donation</h4>
+                  <p>Our donation page is always open at anytime for everyone to help other that needed help.</p>
                 </div>
               </div><!-- End Icon Box -->
 
               <div class="col-xl-4" data-aos="fade-up" data-aos-delay="300">
                 <div class="icon-box d-flex flex-column justify-content-center align-items-center">
                   <i class="bi bi-gem"></i>
-                  <h4>Ullamco laboris ladore pan</h4>
-                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                  <h4>Update to the News</h4>
+                  <p>We always update to the world issues and try our best to help every person in need of food.</p>
                 </div>
               </div><!-- End Icon Box -->
 
               <div class="col-xl-4" data-aos="fade-up" data-aos-delay="400">
                 <div class="icon-box d-flex flex-column justify-content-center align-items-center">
                   <i class="bi bi-inboxes"></i>
-                  <h4>Labore consequatur incidid dolore</h4>
-                  <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                  <h4>Easy to Navigate</h4>
+                  <p>Our system for donation is very simple to help and guide through the process get the donation process smoothly</p>
                 </div>
               </div><!-- End Icon Box -->
 
@@ -126,21 +119,20 @@
 
         <div class="section-header">
           <h2>Contact</h2>
-          <p>Need Help? <span>Contact Us</span></p>
+          <p>Any Queries? <span>Contact Us</span></p>
         </div>
 
         <div class="mb-3">
-          <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+          <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1016892.4432604912!2d101.9952132048108!3d5.380931514128942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31b5d97264fa1a59%3A0x6ec97bbb4a9fbcf0!2sKelantan%2C%20Malaysia!5e0!3m2!1sen!2ssg!4v1674296671551!5m2!1sen!2ssg" frameborder="0" allowfullscreen></iframe>
         </div><!-- End Google Maps -->
-
         <div class="row gy-4">
 
           <div class="col-md-6">
             <div class="info-item  d-flex align-items-center">
               <i class="icon bi bi-map flex-shrink-0"></i>
               <div>
-                <h3>Our Address</h3>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <h3>Our Office Address</h3>
+                <p>Kelantan, Malaysia, MY 123456</p>
               </div>
             </div>
           </div><!-- End Info Item -->
@@ -150,7 +142,7 @@
               <i class="icon bi bi-envelope flex-shrink-0"></i>
               <div>
                 <h3>Email Us</h3>
-                <p>contact@example.com</p>
+                <p>goodfood@sdg.com</p>
               </div>
             </div>
           </div><!-- End Info Item -->
@@ -160,7 +152,7 @@
               <i class="icon bi bi-telephone flex-shrink-0"></i>
               <div>
                 <h3>Call Us</h3>
-                <p>+1 5589 55488 55</p>
+                <p>+012 345678 9999</p>
               </div>
             </div>
           </div><!-- End Info Item -->
@@ -179,28 +171,6 @@
 
         </div>
 
-        <form action="forms/contact.php" method="post" role="form" class="php-email-form p-3 p-md-4">
-          <div class="row">
-            <div class="col-xl-6 form-group">
-              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-            </div>
-            <div class="col-xl-6 form-group">
-              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-            </div>
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-          </div>
-          <div class="form-group">
-            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-          </div>
-          <div class="my-3">
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your message has been sent. Thank you!</div>
-          </div>
-          <div class="text-center"><button type="submit">Send Message</button></div>
-        </form>
         <!--End Contact Form -->
 
       </div>
